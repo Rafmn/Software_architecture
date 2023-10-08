331fac9e39a72f33b70ac6035ac6eaac90cc8d5f
Marat <email.marat@gmail.com>
2023-10-08 13:58:21 +0300

    added HW_3

---
+++ HW_3/dip.java
@@ -0,0 +1,39 @@
+interface IPrinter
+{
+    void Print(string text);
+}
+
+class Book
+{
+    public string Text { get; set; }
+    public IPrinter Printer { get; set; }
+    public Book(IPrinter printer)
+    {
+        this.Printer = printer;
+    }
+        public void Print()
+    {
+        Printer.Print(Text);
+    }
+}
+
+class ConsolePrinter : IPrinter
+{
+    public void Print(string text)
+    {
+        Console.WriteLine("Печать на консоли");
+    }
+}
+
+class HtmlPrinter : IPrinter
+{
+    public void Print(string text)
+    {
+        Console.WriteLine("Печать в html");
+    }
+}
+
+Book book = new Book(new ConsolePrinter());
+book.Print();
+book.Printer = new HtmlPrinter();
+book.Print();
